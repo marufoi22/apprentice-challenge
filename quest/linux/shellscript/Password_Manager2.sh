8db1d541b1db439a1997b74da20d7b765b63af76
yukimaenosono <m.y.882soccer@gmail.com>
2024-03-31 19:47:02 +0900

    Get Passwordの処理を追加

--- quest/linux/shellscript/Password_Manager2.sh
+++ quest/linux/shellscript/Password_Manager2.sh
@@ -8,5 +8,21 @@ if [ "$input" = "Add Password" ]; then
         read -p "ユーザー名を入力してください:" UserName
         read -p "パスワードを入力してください:" PassWord
         echo "$ServiceName;$UserName;$PassWord" >> PASS.conf
+elif [ "$input" = "Get Password" ]; then
+        read -p "サービス名を入力してください:" ServiceName 
+        while IFS=';' read -r -a elements; do
+		if [ "${elements[0]}" = "$ServiceName" ]; then
+	    		ServiceName1="${elements[0]}"
+	    		   UserName1="${elements[1]}"
+	    		   PassWord1="${elements[2]}"
+		fi
+	done < PASS.conf
+
+        if [ -n "$ServiceName1" ]; then 
+		echo "$ServiceName1"
+		echo "$UserName1"
+        	echo "$PassWord1"
+	else
+		echo "そのサービスは登録されていません"
+	fi
 fi
-echo "Thank you"
