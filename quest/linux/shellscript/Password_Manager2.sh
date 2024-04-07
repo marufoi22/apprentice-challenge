10d3fbecebda3b2a895d6250f1c01e559449de0c
yukimaenosono <m.y.882soccer@gmail.com>
2024-04-07 15:26:24 +0900

    パスワードファイル名を変数に格納

--- quest/linux/shellscript/Password_Manager2.sh
+++ quest/linux/shellscript/Password_Manager2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+encrypted_passfile=~/Documents/passsv.asc
+passfile=~/Documents/passsv
 source ./mail_address.conf
 
 echo 'パスワードマネージャーへようこそ!'
@@ -6,21 +8,21 @@ echo 'パスワードマネージャーへようこそ!'
 read -p "次の選択肢から入力してください(Add Password/Get Password/Exit):" input
 
 if [ "$input" = "Add Password" ]; then
-	if   [ -e PASS.conf.asc ]; then
-        	gpg PASS.conf.asc > /dev/null && rm PASS.conf.asc
+	if   [ -e $encrypted_passfile ]; then
+        	gpg $encrypted_passfile > /dev/null && rm $encrypted_passfile
 	else
-		touch PASS.conf
-		echo "PASS.confを新規作成しました"
+		touch $passfile
+		echo "$passfileを新規作成しました"
 	fi
         read -p "サービス名を入力してください:" ServiceName
         read -p "ユーザー名を入力してください:" UserName
         read -p "パスワードを入力してください:" PassWord
-        echo "$ServiceName;$UserName;$PassWord" >> PASS.conf
-        gpg -e -a -r "$mail_address" PASS.conf
-	rm PASS.conf
+        echo "$ServiceName;$UserName;$PassWord" >> $passfile
+        gpg -e -a -r "$mail_address" $passfile
+	rm $passfile
 elif [ "$input" = "Get Password" ]; then
-	if [ -e PASS.conf.asc ]; then
-		gpg PASS.conf.asc > /dev/null && rm PASS.conf.asc
+	if [ -e $encrypted_passfile ]; then
+		gpg $encrypted_passfile > /dev/null && rm $encrypted_passfile
         	read -p "サービス名を入力してください:" ServiceName 
         	while IFS=';' read -r -a elements; do
 			if [ "${elements[0]}" = "$ServiceName" ]; then
@@ -28,7 +30,7 @@ elif [ "$input" = "Get Password" ]; then
 	    		   	UserName1="${elements[1]}"
 	    		   	PassWord1="${elements[2]}"
 			fi
-		done < PASS.conf
+		done < $passfile
 
         	if [ -n "$ServiceName1" ]; then 
 			echo "$ServiceName1"
@@ -37,10 +39,10 @@ elif [ "$input" = "Get Password" ]; then
 		else
 			echo "そのサービスは登録されていません"
 		fi
-		gpg -e -a -r "$mail_address" PASS.conf
-		rm PASS.conf
+		gpg -e -a -r "$mail_address" "$passfile"
+		rm $passfile
 	else
-	        echo "PASS.confがありません"
+	        echo "$passfileがありません"
 	fi
 elif [ "$input" = "Exit" ]; then
 	echo "Thank you!"
